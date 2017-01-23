a1ef296ab0d016601e62a9b2690f74d918787fd2
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-01-23 10:26:11 -0700

    added 'ifExists' to Table drop

--- migrations/20170123093818_equipment.js
+++ migrations/20170123093818_equipment.js
@@ -9,5 +9,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('equipment');
+  return knex.schema.dropTableIfExists('equipment');
 };
